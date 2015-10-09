17088fb678628bce6b298576849e0c926f4f0faf
lanqinglong <lanqinglong@huawei.com>
2015-10-09 20:11:36 +0800

    modify

--- ONOS/Teston/CI/onosfunctest.py
+++ ONOS/Teston/CI/onosfunctest.py
@@ -10,8 +10,19 @@ lanqinglong@huawei.com
 from adapters.client import client 
 
 if __name__=="__main__":
-
+    self.masterusername = "root"
+    self.masterpassword = "root"
+    self.agentusername = "root"
+    self.agentpassword = "root"
+    self.OCT = '189.42.8.99'
+    self.OC1 = '189.42.8.101'
+    self.OC2 = '189.42.8.102'
+    self.OC3 = '189.42.8.103'
+    self.OCN = '189.42.8.104'
+    self.OCN2 = '189.42.8.105'
+    self.localhost = self.OCT
+    
     main = client()
     main.onosbasic()
-    main.RunScript("FUNCvirNetNB",masterusername,masterpassword)
-    main.RunScript("FUNCovsdbtest",masterusername,masterpassword)
+    main.RunScript("FUNCvirNetNB",self.masterusername,self.masterpassword)
+    main.RunScript("FUNCovsdbtest",self.masterusername,self.masterpassword)
